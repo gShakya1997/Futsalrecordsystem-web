40736a1c295947f30446e85ee47c7bf23d725bda
gunjan <gunjan.shakya3@gmail.com>
2020-02-08 19:04:14 +0545

    User registration form and registered successfully

---
+++ src/Components/Registration/Registration.js
@@ -0,0 +1,269 @@
+import React from "react";
+import {
+    Form,
+    Button,
+    FormLabel,
+    ButtonToolbar,
+    ToggleButtonGroup,
+    ToggleButton
+} from "react-bootstrap";
+import {
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCardHeader,
+    MDBIcon
+} from "mdbreact";
+import Axios from "axios";
+import { Redirect } from "react-router-dom";
+
+class FutsalRegistration extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            futsalName: "",
+            futsalAddress: "",
+            futsalEmail: "",
+            futsalPhone: "",
+            futsalPassword: "",
+            futsalCPassword: "",
+            futsalOpeningTime: "",
+            futsalClosingTime: "",
+            futsalPrice: "",
+            username: "",
+            address: "",
+            email: "",
+            phone: "",
+            password: "",
+            cpassword: "",
+            gender: "Male",
+            isRegistered: false
+        }
+    }
+
+    // Handler for futsal
+    futsalNameHandler = (e) => {
+        this.setState({ futsalName: e.target.value });
+    }
+
+    futsalAddressHandler = (e) => {
+        this.setState({ futsalAddress: e.target.value });
+    }
+
+    futsalEmailHandler = (e) => {
+        this.setState({ futsalEmail: e.target.value });
+    }
+
+    futsalPhoneHandler = (e) => {
+        this.setState({ futsalPhone: e.target.value });
+    }
+
+    futsalPasswordHandler = (e) => {
+        this.setState({ futsalPassword: e.target.value });
+    }
+
+    futsalCPasswordHandler = (e) => {
+        this.setState({ futsalCPassword: e.target.value });
+    }
+
+    futsalOpeningTimeHandler = (e) => {
+        this.setState({ futsalOpeningTime: e.target.value });
+    }
+
+    futsalClosingTimeHandler = (e) => {
+        this.setState({ futsalClosingTime: e.target.value });
+    }
+
+    futsalPriceHandler = (e) => {
+        this.setState({ futsalPrice: e.target.value });
+    }
+
+    //submit handler for futsal
+    submitHandlerFutsal = (e) => {
+        e.preventDefault();
+        var data = {
+            futsalName: this.state.futsalName,
+            futsalAddress: this.state.futsalAddress,
+            futsalEmail: this.state.futsalEmail,
+            futsalPhone: this.state.futsalPhone,
+            futsalPassword: this.state.futsalPassword,
+            futsalCPassword: this.state.futsalCPassword,
+            futsalOpeningTime: this.state.futsalOpeningTime,
+            futsalClosingTime: this.state.futsalClosingTime,
+            futsalPrice: this.state.futsalPrice
+        }
+        var headers = {
+            "Content-type": "application/json"
+        }
+        Axios.post(
+            "http://localhost:3007/futsal/register",
+            data,
+            headers
+        ).then((response) => {
+            console.log(response);
+        }).then((data) => {
+            console.log(data);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
+    //Handler for user
+    usernameHandler = (e) => {
+        this.setState({ username: e.target.value });
+    }
+
+    addressHandler = (e) => {
+        this.setState({ address: e.target.value });
+    }
+
+    emailHandler = (e) => {
+        this.setState({ email: e.target.value });
+    }
+
+    phoneHandler = (e) => {
+        this.setState({ phone: e.target.value });
+    }
+
+    passwordHandler = (e) => {
+        this.setState({ password: e.target.value });
+    }
+
+    cPasswordHandler = (e) => {
+        this.setState({ cpassword: e.target.value });
+    }
+
+    genderHandler = (e) => {
+        this.setState({ gender: e.target.value });
+    }
+
+    //submit handler for user
+    submitHandlerUser = (e) => {
+        e.preventDefault();
+        var dataUser = {
+            username: this.state.username,
+            address: this.state.address,
+            email: this.state.email,
+            phone: this.state.phone,
+            password: this.state.password,
+            cpassword: this.state.cpassword,
+            gender: this.state.gender,
+            isRegistered: true
+        }
+        console.log(dataUser);
+        Axios.post(
+            "http://localhost:3007/users/register",
+            dataUser
+        ).then((response) => {
+            console.log(response.data);
+            localStorage.setItem("token", response.data.token);
+            <Redirect to="/home"/>
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
+    render() {
+        if (this.state.isRegistered === true) {
+            return <Redirect to="/home" />
+        }
+        return (
+            <MDBContainer>
+                <MDBRow>
+                    {/* Registration form for Futsal Owner */}
+                    <MDBCol md="6">
+                        <Form onSubmit={this.submitHandlerFutsal}>
+                            <MDBCardHeader className="form-header warm-flame-gradient rounded">
+                                <h3 className="my-3">
+                                    <MDBIcon icon="lock" /> Futsal Registration
+                                </h3>
+                            </MDBCardHeader>
+                            <Form.Group controlId="formBasicFutsalName">
+                                <Form.Control type="text" placeholder="Futsal name" value={this.state.futsalName} onChange={this.futsalNameHandler} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicAddress">
+                                <Form.Control type="text" placeholder="Address" value={this.state.futsalAddress} onChange={this.futsalAddressHandler} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Control type="email" placeholder="Email address" value={this.state.futsalEmail} onChange={this.futsalEmailHandler} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPhone">
+                                <Form.Control type="number" placeholder="Phone no" value={this.state.futsalPhone} onChange={this.futsalPhoneHandler} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Control type="password" placeholder="Password" value={this.state.futsalPassword} onChange={this.futsalPasswordHandler} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicCPassword">
+                                <Form.Control type="password" placeholder="Confirm Password" value={this.state.futsalCPassword} onChange={this.futsalCPasswordHandler} />
+                            </Form.Group>
+                            <Form.Text className="text-muted">
+                                HH:MM am/pm
+                            </Form.Text>
+                            <Form.Group controlId="formBasicOpeningTime">
+                                <Form.Control type="text" placeholder="Opening Time" value={this.state.futsalOpeningTime} onChange={this.futsalOpeningTimeHandler} />
+                            </Form.Group>
+                            <Form.Text className="text-muted">
+                                HH:MM am/pm
+                            </Form.Text>
+                            <Form.Group controlId="formBasicClosingTime">
+                                <Form.Control type="text" placeholder="Closing Time" value={this.state.futsalClosingTime} onChange={this.futsalClosingTimeHandler} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicFutsalPrice">
+                                <Form.Control type="number" placeholder="Price per hour" value={this.state.futsalPrice} onChange={this.futsalPriceHandler} />
+                            </Form.Group>
+                            <div className="text-center py-4 mt-3">
+                                <Button variant="primary" type="submit">
+                                    Register as Futsal Owner
+                                </Button>
+                            </div>
+                        </Form>
+                    </MDBCol>
+
+                    {/* Registration form for user */}
+                    <MDBCol md="6">
+                        <Form onSubmit={this.submitHandlerUser}>
+                            <MDBCardHeader className="form-header warm-flame-gradient rounded">
+                                <h3 className="my-3">
+                                    <MDBIcon icon="lock" /> User Registration
+                                </h3>
+                            </MDBCardHeader>
+                            <Form.Group controlId="formBasicUsername">
+                                <Form.Control type="text" placeholder="Username" value={this.state.username} onChange={this.usernameHandler} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicUserAddress">
+                                <Form.Control type="text" placeholder="Address" value={this.state.address} onChange={this.addressHandler} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicUserEmail">
+                                <Form.Control type="email" placeholder="Email address" value={this.state.email} onChange={this.emailHandler} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicUserPhone">
+                                <Form.Control type="number" placeholder="Phone no" value={this.state.phone} onChange={this.phoneHandler} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicUserPassword">
+                                <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.passwordHandler} />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicUserCPassword">
+                                <Form.Control type="password" placeholder="Confirm Password" value={this.state.cpassword} onChange={this.cPasswordHandler} />
+                            </Form.Group>
+                            <FormLabel>Gender</FormLabel>
+                            <ButtonToolbar>
+                                <ToggleButtonGroup type="radio" name="options">
+                                    <ToggleButton value={"Male"} checked={this.state.gender === "Male"} onChange={this.genderHandler}>Male</ToggleButton>
+                                    <ToggleButton value={"Female"} checked={this.state.gender === "Female"} onChange={this.genderHandler}>Female</ToggleButton>
+                                    <ToggleButton value={"Others"} checked={this.state.gender === "Others"} onChange={this.genderHandler}>Others</ToggleButton>
+                                </ToggleButtonGroup>
+                            </ButtonToolbar>
+                            <div className="text-center py-4 mt-3">
+                                <Button variant="primary" type="submit">
+                                    Register as User
+                                </Button>
+                            </div>
+                        </Form>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        )
+    }
+}
+
+export default FutsalRegistration;
